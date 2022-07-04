5031232608d985724a6a78a22300fa43971a1aaf
opensuse <opensuse@localhost.localdomain>
2022-07-04 21:21:57 +0200

    Added a test png to see how to continue forwards

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-
-    // Testing png loading:
-
 
     w.show();
     return a.exec();
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,19 +1,29 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QLabel>
+#include <QLineEdit>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    //Initial test of png rendering
-
-    QString url = R"(/home/opensuse/Documents/Keyboard.png)";
+    //Get the desired png
+    QString url = ("/home/opensuse/keyboard-listener/Esc_Key_Dark.png");
     QPixmap img(url);
 
-    ui->label->setPixmap(img);
-    ui->label->setScaledContents(true);
+    //Create a label for that image
+    QLabel *label = new QLabel(this);
+
+    label->setPixmap(img);
+    label->setScaledContents(false);
+
+    //Set it's position
+    label->setGeometry(0,0,img.width(),img.height());
+
+    //Make background transparent
     setAttribute(Qt::WA_TranslucentBackground, true);
 }
 
