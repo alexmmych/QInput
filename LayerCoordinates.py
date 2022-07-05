cdf473fcdb94bcdfbeb1c384c899e0cabf8b59a1
opensuse <opensuse@localhost.localdomain>
2022-07-05 21:08:26 +0200

    Fixed the python addon to print to file correctly

--- LayerCoordinates.py
+++ LayerCoordinates.py
@@ -3,21 +3,21 @@
 from gimpfu import *
 import os
 
+f1 = open('./layer-map.txt', 'w')
+
 def getLayers(g):
-    f1 = open('./layer-map.txt', 'w')
     for l in g.layers:
         if isinstance(l,gimp.GroupLayer):
             getLayers(l)
         else:
             print l.name, l.offsets
             print >> f1, l.name, l.offsets
-    message = "Successfully printed the coordinates to: " + os.path.realpath(f1.name)
-    pdb.gimp_message(message)
-    f1.close()
-
 def start():
     image=gimp.image_list()[0]
     getLayers(image)
+    message = "Successfully printed the coordinates to: " + os.path.realpath(f1.name)
+    pdb.gimp_message(message)
+    f1.close()
 
 register(
   "LayerCoordinates",
