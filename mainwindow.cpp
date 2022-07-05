5f41611aa4d94a18f4cecb87134d8b1cafe863a2
opensuse <opensuse@localhost.localdomain>
2022-07-05 22:08:37 +0200

    Added the keyboard images and configured basic ifstream functionality with layer-map.txt

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,7 +2,13 @@
 #include "./ui_mainwindow.h"
 
 #include <QLabel>
+#include <QString>
 #include <QLineEdit>
+
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -10,8 +16,26 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    //Open Key map
+    std::ifstream file("layer-map.txt");
+
+    std::vector<std::string> names;
+    std::string input;
+
+    while (file >> input){ //return file
+        names.push_back(input);
+    }
+
     //Get the desired png
-    QString url = ("/home/opensuse/keyboard-listener/Esc_Key_Dark.png");
+    QString resources = "/home/opensuse/keyboard-listener/resources/Dark/";
+
+    resources += QString::fromStdString(names.at(0));
+
+    for (std::string name : names) {
+        std::cout << name << std::endl;
+    }
+
+    QString url = (resources);
     QPixmap img(url);
 
     //Create a label for that image
